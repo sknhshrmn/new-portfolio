2e0fad050f934af285b6498db861f99412f69101
sknhshrmn <sakinahshahriman@gmail.com>
2025-03-19 06:06:11 +0800

    add icon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,7 +8,6 @@ import { notFound } from "next/navigation";
 import Menu from "@/src/components/Layout/Menu";
 import ToggleLanguage from "@/src/components/Layout/ToggleLanguage";
 import Footer from "@/src/components/Layout/Footer";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Sakinah Shahriman",
@@ -31,9 +30,6 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
-      <Head>
-        <link rel="shortcut icon" href="/images/icon.ico" sizes="any" />
-      </Head>
       <body
         className={`${montserrat.variable} ${poppins.variable} bg-background text-foreground`}
       >
